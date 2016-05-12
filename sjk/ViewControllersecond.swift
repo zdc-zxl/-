657bd49f45d7e21539d309c66d0a59e9ac5f17be
zdc-zxl <dllm@123.local>
2016-05-12 11:51:36 +0800

    乒乓球计分软件
    5.12

--- sjk/ViewControllersecond.swift
+++ sjk/ViewControllersecond.swift
@@ -15,9 +15,9 @@ class ViewControllersecond: UIViewController {
         super.viewDidLoad()
         db = SQLiteDB.sharedInstance()
         //如果表还不存在则创建表（其中uid为自增主键）
-        db.execute("create table if not exists t_cound(uid integer primary key,fance varchar(20))")
+        db.execute("create table if not exists t_cound(uid integer primary key,red varchar(20),blue varchar(20),win varchar(20))")
         //如果有数据则加载
-        initUser()
+       // initUser()
 
         // Do any additional setup after loading the view.
     }
@@ -25,18 +25,30 @@ class ViewControllersecond: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-        saveUser()
+        //saveUser()
     }
     
-    @IBOutlet weak var reed: UITextField!
-
-    @IBOutlet weak var write: UITextField!
-    func initUser() {
+    //@IBOutlet weak var show: UILabel!
+   // @IBOutlet weak var reed: UITextField!
+    
+    @IBOutlet weak var show: UILabel!
+    @IBOutlet weak var redman: UITextField!
+  //  @IBOutlet weak var write: UITextField!
+    
+    @IBOutlet weak var blueman: UITextField!
+    @IBOutlet weak var rn: UILabel!
+    @IBOutlet weak var bn: UILabel!
+    var tep=0;
+    var r=0;
+    var b2=0;
+    /*
+   func initUser() {
         let data = db.query("select * from t_cound")
         if data.count > 0 {
             //获取最后一行数据显示
             let user = data[data.count - 1]
-            write.text = user["fance"] as? String
+          
+            // Redwin.text = user["fance"] as? String
             //txtMobile.text = user["mobile"] as? String
         }
     }
@@ -49,6 +61,8 @@ class ViewControllersecond: UIViewController {
         let result = db.execute(sql)
         print(result)
     }
+*/
+    /*
     @IBAction func du(sender: AnyObject) {
         //插入数据库，这里用到了esc字符编码函数，其实是调用bridge.m实现的
         let fance = self.reed.text!
@@ -69,7 +83,7 @@ class ViewControllersecond: UIViewController {
            
         }
 
-    }
+    }*/
     @IBOutlet weak var redf: UILabel!//红方得分
     @IBOutlet weak var bluef: UILabel!
     //蓝方得分
@@ -80,6 +94,8 @@ class ViewControllersecond: UIViewController {
     var a1=0;
     var b1=0;
     @IBAction func redbutton(sender: AnyObject) {
+        if(tep==0)
+        {
         a++
         if(a<11)
         {
@@ -98,10 +114,24 @@ class ViewControllersecond: UIViewController {
            a=0;
             redf.text!=""
         }
+        if(a1==3)
+        {
+            show.text!+="选手： "
+            show.text!+=redman.text!;
+            show.text!+=" 获得本场比赛胜利"
+            tep=1;
+        }
+        }
+        else
+        {
+            redf.text!=""
+        }
         
     }
     
     @IBAction func bluef(sender: AnyObject) {
+        if(tep==0)
+        {
         b++
        // bluef.text!="\(b)"
         
@@ -122,6 +152,19 @@ class ViewControllersecond: UIViewController {
             b=0;
             bluef.text!=""
         }
+        if(b1==3)
+        {
+            show.text!+="选手： "
+            show.text!+=blueman.text!;
+            show.text!+=" 获得本场比赛胜利"
+            tep=1;
+            //show.text!=redman.text!;
+        }
+        }
+        else
+        {
+           bluef.text!=""
+        }
         
 
     }
@@ -131,6 +174,16 @@ class ViewControllersecond: UIViewController {
 
         b=0;
         bluef.text!=""
+    }
+    
+    @IBAction func redno(sender: AnyObject) {
+        r++
+        rn.text!="\(r)"
+    }
+    
+    @IBAction func blueno(sender: AnyObject) {
+        b2++
+        bn.text!="\(b2)"
     }
     /*
     // MARK: - Navigation
